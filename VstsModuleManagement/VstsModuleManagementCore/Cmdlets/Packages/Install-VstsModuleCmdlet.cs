ea018e67ec0c9bd09e87d16279ba051217528ee9
Josh Irwin <joirwi@microsoft.com>
2018-05-17 19:06:05 -0700

    Continuing work

---
+++ VstsModuleManagement/VstsModuleManagementCore/Cmdlets/Packages/Install-VstsModuleCmdlet.cs
@@ -0,0 +1,31 @@
+﻿// ***********************************************************************
+// Assembly         : VstsModuleManagementCore
+// Author           : joirwi
+// Created          : 5/17/2018 7:01:19 PM
+//
+// Last Modified By : joirwi
+// Last Modified On : 5/17/2018 7:01:19 PM
+// ***********************************************************************
+// <copyright file="Install-VstsModuleCmdlet.cs" company="Microsoft">
+//     Copyright ©  2018
+// </copyright>
+// <summary></summary>
+// **********************************************************************
+
+namespace VstsModuleManagementCore.Cmdlets.Packages
+{
+    /// <summary>
+    /// Class Install_VstsModuleCmdlet.
+    /// </summary>
+    /// <seealso cref="AbstractBaseCmdlet" />
+    public class Install_VstsModuleCmdlet : AbstractBaseCmdlet
+    {
+        /// <summary>
+        /// Processes the command.
+        /// </summary>
+        protected override void ProcessCommand()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ VstsModuleManagement/VstsModuleManagementCore/Cmdlets/Packages/UninstallVstsModuleCmdlet.cs
@@ -0,0 +1,31 @@
+﻿// ***********************************************************************
+// Assembly         : VstsModuleManagementCore
+// Author           : joirwi
+// Created          : 5/17/2018 7:00:02 PM
+//
+// Last Modified By : joirwi
+// Last Modified On : 5/17/2018 7:00:02 PM
+// ***********************************************************************
+// <copyright file="UninstallVstsModuleCmdlet.cs" company="Microsoft">
+//     Copyright ©  2018
+// </copyright>
+// <summary></summary>
+// **********************************************************************
+
+namespace VstsModuleManagementCore.Cmdlets.PrivateCommands
+{
+    /// <summary>
+    /// Class UninstallVstsModuleCmdlet.
+    /// </summary>
+    /// <seealso cref="AbstractBaseCmdlet" />
+    public class UninstallVstsModuleCmdlet : AbstractBaseCmdlet
+    {
+        /// <summary>
+        /// Processes the command.
+        /// </summary>
+        protected override void ProcessCommand()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
