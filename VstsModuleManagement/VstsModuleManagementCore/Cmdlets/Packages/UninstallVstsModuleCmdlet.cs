21da2533bb0cd876412a528cae0012bbd13fa1e7
Josh Irwin <joirwi@microsoft.com>
2018-05-20 14:11:59 -0700

    Basic code to make Uninstall-VstsModule work

--- VstsModuleManagement/VstsModuleManagementCore/Cmdlets/Packages/UninstallVstsModuleCmdlet.cs
+++ VstsModuleManagement/VstsModuleManagementCore/Cmdlets/Packages/UninstallVstsModuleCmdlet.cs
@@ -14,18 +14,95 @@
 
 namespace VstsModuleManagementCore.Cmdlets.PrivateCommands
 {
+    using System;
+    using System.Management.Automation;
+
+    using VstsModuleManagementCore.Extensions;
+    using VstsModuleManagementCore.Utilities;
+
     /// <summary>
     /// Class UninstallVstsModuleCmdlet.
     /// </summary>
     /// <seealso cref="AbstractBaseCmdlet" />
+    [Cmdlet(VerbsLifecycle.Uninstall, "VstsModule")]
     public class UninstallVstsModuleCmdlet : AbstractBaseCmdlet
     {
+        [Parameter(Mandatory = true)]
+        public string Name { get; set; }
+
+        [Parameter]
+        public SwitchParameter AllVersions { get; set; }
+
+        [Parameter]
+        public Version MinimumVersion { get; set; }
+
+        [Parameter]
+        public Version MaximumVersion { get; set; }
+
+        [Parameter]
+        public Version RequiredVersion { get; set; }
+
+        private int DeterminedCodePath;
+
+        protected override void BeginProcessingCommand()
+        {
+            if (this.AllVersions)
+            {
+                this.DeterminedCodePath = 1;
+            }
+            else if (this.RequiredVersion != null)
+            {
+                this.DeterminedCodePath = 2;
+            }
+            else if (this.MinimumVersion != null && this.MaximumVersion != null)
+            {
+                this.DeterminedCodePath = 3;
+            }
+        }
+
         /// <summary>
         /// Processes the command.
         /// </summary>
         protected override void ProcessCommand()
         {
-            throw new System.NotImplementedException();
+            var param = this.CreateParameterDictionary("Name", this.Name);
+
+            switch (this.DeterminedCodePath)
+            {
+                case 1:
+
+                    {
+                        param.Add("AllVersions", this.AllVersions);
+                        PSUtils.InvokePSCommand("Uninstall-Module", param);
+                        break;
+                    }
+
+                case 2:
+
+                    {
+                        param.Add("RequiredVersion", this.RequiredVersion);
+                        PSUtils.InvokePSCommand("Uninstall-Module", param);
+                        break;
+                    }
+
+                case 3:
+
+                    {
+                        param.AddParameter("MinimumVersion", this.MinimumVersion)
+                             .AddParameter("MaximumVersion", this.MaximumVersion);
+                        PSUtils.InvokePSCommand("Uninstall-Module", param);
+                        break;
+                    }
+
+                default:
+
+                    {
+                        this.WriteInformation("Your input is not understood.  Please see the help section on this cmdlet below.", null);
+                        param.Add("Name", "Uninstall-VstsModule");
+                        this.WriteObject(PSUtils.InvokePSCommand<object>("Get-Help", param));
+                        break;
+                    }
+            }
         }
     }
 }
